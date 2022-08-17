2b28670db0c1f8ba98b25f22bb2d326dad779ad7
Joseph Kumher <joekumher@gmail.com>
2022-08-17 13:08:37 -0400

    created a prompt that parses integers and retruns an error if the minimum isnt reached

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+//Character sets
+var passwordText = document.querySelector("#password");
+var charsetUpper = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var charsetLower = "abcdefghijklmnopqrstuvwxyz";
+var charsetSpecial =  "!#$%&'()*+-./:;<=>?@[_`{|}~";
+var charsetNumber = "0123456789";
 // Password Generation
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var charsetUpper = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-  var charsetLower = "abcdefghijklmnopqrstuvwxyz";
-  var charsetSpecial =  "!#$%&'()*+-./:;<=>?@[_`{|}~";
-  var charsetNumber = "0123456789";
+function generatePassword(Length) {
+  var password = '';
+  var passwordchar ="";
 
-  passwordText.value = password;
+  var passwordLengthUser = prompt("How long would you like your password to be? It must be between 8-124 character.");
+  passwordLengthUser = parseInt(passwordLengthUser);
+
+if (passwordLengthUser < 8) {
+alert("Password must have more than 7 characters!");
+return ("");
+}
 
 }
 
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 //Password Character Criteria Prompts
 
