36aedea6f0f00e2fd0681c82d6e1e6df3fe3ac49
Joseph Kumher <joekumher@gmail.com>
2022-08-16 20:32:27 -0400

    created a new variable called password length and set the value of password length to whatever specified length will be

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,6 @@
 
 function createPassword(length) {
   let password = ''; 
+  const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let passwordLength = length;
+}
