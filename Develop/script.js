0287194e28dcf620c8c152bbda5d6e5a507316a2
Joseph Kumher <joekumher@gmail.com>
2022-08-16 20:28:55 -0400

    created a new empty string called password

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar
-var uppercaseChar
-var numericalChar
-var specialChar
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function createPassword(length) {
+  let password = ''; 
