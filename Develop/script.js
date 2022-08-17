b2dd681c56f4f9881e47c922fec18ca4465f5dec
Joseph Kumher <joekumher@gmail.com>
2022-08-16 20:36:25 -0400

    created new variable array and used Uint32Array to make sure everything generated is positive and left its arguement at length also used crypto.getrandomvalues because its more random than getrandomvalues

--- Develop/script.js
+++ Develop/script.js
@@ -3,4 +3,15 @@ function createPassword(length) {
   let password = ''; 
   const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let passwordLength = length;
+
+  const array = new Uint32Array(length);
+  window.crypto.getRandomValues(array);
+
+
+
+
+
+
+
+
 }
